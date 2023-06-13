4804c004a8a0a521ec013daf8adaf439b144dcba
EmranAli-15 <mohammademranali15@gmail.com>
2023-06-13 20:47:41 +0600

    user can get single data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
@@ -29,6 +29,18 @@ async function run() {
         app.post('/addTasks', async (req, res) => {
             const task = req.body;
             const result = await taskCollection.insertOne(task);
+            res.send(result);
+        })
+
+        app.get('/allTasks', async (req, res) => {
+            const result = await taskCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/singleTasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await taskCollection.findOne(query);
             res.send(result);
         })
 
